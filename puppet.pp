c1985f916698c5856cd375c80c89eee1c69daa42
Stephen Mcgroarty <Stephen.Mcgroarty@aljazeeera.net>
2015-01-30 17:24:04 -0800

    My second puppet file, for controlling the puppet

---
+++ puppet.pp
@@ -0,0 +1,11 @@
+class accounts::puppet {
+
+    file { '/etc/puppet.conf':
+        ensure => file,
+        mode   => '644',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/accounts/puppet.conf.erb'
+    }
+}
+
